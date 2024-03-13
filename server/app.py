51fb657bc925723a5a9fd4f4d498649a9e62ce79
Shashank <shashankgupta9248@gmail.com>
2024-03-13 17:47:24 +0530

    :truck:: backend done for recommendation from image to text and tts

--- server/app.py
+++ server/app.py
@@ -1,3 +1,92 @@
-from flask import Flask, request, jsonify
+import os
+from flask_pymongo import PyMongo
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from dotenv import load_dotenv, find_dotenv
+import requests
+from langchain.llms.cohere import Cohere
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
+
+app.config['MONGO_URI'] = os.environ['MONGO_URI']
+CORS(app)
+db = PyMongo(app).db
+llm = Cohere(cohere_api_key=os.environ['COHERE_API_KEY'])
+
+
+@app.route('/', methods=['GET', 'POST'])
+def application():
+    return '<center style="height: 100dvh"><h1>Backend Link</h1></center>'
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    payload = request.get_json()
+    print(payload)
+    user = db.users.find_one({'email': payload['email']})
+    if user is None:
+        db.users.insert_one({
+            'name': payload['name'],
+            'email': payload['email'],
+            'password': payload['password']
+        })
+        return jsonify({'output': True})
+    return jsonify({'output': 'EmailId already registered'})
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    payload = request.get_json()
+    user = db.users.find_one({'email': payload['email']})
+    if user is not None:
+        if user['password'] == payload['password']:
+            return jsonify({'output': True})
+        return jsonify({'output': "Incorrect password"})
+    return jsonify({'output': 'No such user found'})
+
+
+@app.route('/get_data_from_url', methods=['POST'])
+def get_data_from_url():
+    image = request.get_json()["image"]
+    url = "https://picarta.ai/classify"
+    api_token = "EKKO8R3GUHA81XJYJMDG"
+    headers = {"Content-Type": "application/json"}
+
+    # Process image based on input type (file or URL)
+    if image:
+        payload = {"TOKEN": api_token, "IMAGE": image}
+        response = requests.post(url, headers=headers, json=payload)
+        print(response.json)
+        # Process API response
+        if response.status_code == 200:
+            result = response.json()
+            print(result)
+            # pass it to LLM to give a better output and suggestions to travel in the place
+            answer = llm.predict(f"Firstly tell the user which area they are searching according to the image they uploaded. The country is {result['ai_country']}. "
+                                 f"Suggest destinations and cuisines to the user for the country: {result['ai_country']}. "
+                                 f"The user is planning on travelling to this country. "
+                                 f"Your job is to be a travel guide and suggest places to visit and cuisines to try. "
+                                 f"The user is interested to visit {result['city'] if result['city'] else 'some city in the country'} and {result['province'] if result['province'] else 'some province in the country'}.")
+
+            return jsonify(answer)
+        else:
+            print("Request failed with status code:", response.status_code)
+            return jsonify({'output': 'Request failed with status code: ' + str(response.status_code)})
+    else:
+        return jsonify({'output': 'No image provided'})
+
+@app.route('/tts', methods=['POST'])
+def text_to_speech():
+    url = 'https://api.fpt.ai/hmi/tts/v5'
+    payload = request.get_json()["text"]
+    headers = {
+        'api-key': 'Alh8eML5PTBfJu3153eMPRBtJGGzC7SV',
+        'speed': '',
+        'voice': 'banmai'
+    }
+    response = requests.request('POST', url, data=payload.encode('utf-8'), headers=headers)
+    print(response.text)
+    return jsonify({ "text": response.text})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
